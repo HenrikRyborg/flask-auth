d2a8ae7a75901bdf698b296caee3f7d1bf182154
Henrik Ryborg Poulsen <HEPO@seges.dk>
2018-01-10 13:54:16 +0100

    commit

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Email, EqualTo
 
-# from ..models import Employee
+from ..models import account
 
 class registerAccountForm(FlaskForm):
     accountName = StringField('Account Name', validators=[DataRequired()])
@@ -11,9 +11,9 @@ class registerAccountForm(FlaskForm):
     confirmPassword = PasswordField('Confirm Password')
     submit = SubmitField('Register')
 
-    # def validate_email(self, field):
-    #     if Employee.query.filter_by(email=field.data).first():
-    #         raise ValidationError('Email is already in use.')
+    def validateAccount(self, field):
+        if account.query.filter_by(name=field.data).first():
+            raise ValidationError('Account is already in use.')
 
 class setPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirmPassword')])
